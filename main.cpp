f171ec0ce0085434d4cda38f405b2ccdad8f7536
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2021-12-14 21:21:55 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "coba coba github ges" << endl;
+    cout << "coba coba github ges, 1, 2, 3" << endl;
     return 0;
 }
