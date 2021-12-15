0027418619fa067003445a1948b1221365571205
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2021-12-15 20:28:41 +0700

    change

--- Streaming.h
+++ Streaming.h
@@ -1,23 +1,34 @@
 #ifndef STREAMING_H_INCLUDED
 #define STREAMING_H_INCLUDED
+
 #define first(L) L.first
 #define last(L) L.last
+
+#define infoParent(P) P -> infoParent
+#define nextParent(P) P -> NextParent
+
+#define infoChild(P) P -> infoChild
+#define nextChild(P) P-> NextChild
+
 typedef struct elm_parent *adr_parent;
 typedef struct elm_children *adr_children;
 struct parent{
     string genre;
     int total;
 };
+struct children{
+    string namFilm;
+    int rating;
+    string sutradara;
+};
 struct elm_parent{
-    parent info;
-    adr_parent next;
+    parent infoParent;
+    adr_parent nextParent;
     adr_children nextChild;
 };
 struct elm_children {
-    string namaFilm;
-    int rating;
-    string sutradara;
-    adr_children Next;
+    children infoChildr;
+    adr_children NextChild;
 };
 struct mll {
     adr_parent first;
