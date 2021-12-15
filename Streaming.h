39c75b33377650ecd568b0c59a53741ac3db6618
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2021-12-15 20:17:44 +0700

    Update Streaming.h

--- Streaming.h
+++ Streaming.h
@@ -2,6 +2,29 @@
 #define STREAMING_H_INCLUDED
 #define first(L) L.first
 #define last(L) L.last
+typedef struct elm_parent *adr_parent;
+typedef struct elm_children *adr_children;
+struct parent{
+    string genre;
+    int total;
+};
+struct elm_parent{
+    parent info;
+    adr_parent next;
+    adr_children nextChild;
+};
+struct elm_children {
+    string namaFilm;
+    int rating;
+    string sutradara;
+    adr_children Next;
+};
+struct mll {
+    adr_parent first;
+    adr_parent last;
+};
+
+
 
 
 #endif // STREAMING_H_INCLUDED
